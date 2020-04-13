24bfeeadac2d668a64ce85a5f6e74a8412f024b7
marrodri <laza.mar64@gmail.com>
2020-04-12 19:06:15 -0700

    file comparisons fixed and time comparison fixed

--- ft_ls.c
+++ ft_ls.c
@@ -7,10 +7,12 @@ t_file_data *init_file_data(char *file_name, char *cur_direct)
 	t_stat		stat_file;
 	t_file_data *new_fd;
 	char		*bit_size_str;
+	time_t	new_mtime;
 
 	new_fd = malloc(sizeof(t_file_data));
 	file_path = append_directory(cur_direct, file_name);
 	stat(file_path, &stat_file);
+	new_mtime = stat_file.st_mtime;
 	new_fd->file_name = file_name;
 	new_fd->file_path_name = file_path;
 	bit_size_str = filesize_to_string(stat_file.st_size);
@@ -19,12 +21,11 @@ t_file_data *init_file_data(char *file_name, char *cur_direct)
 	new_fd->l_len_padding = ft_strlen(bit_size_str);
 	new_fd->bits_size_output = bit_size_str;
 	new_fd->file_size = stat_file.st_size;
-	new_fd->mod_time = stat_file.st_mtime;
+	new_fd->mod_time = new_mtime;
 	new_fd->user_id = stat_file.st_uid;
 	new_fd->group_id = stat_file.st_gid;
 	return (new_fd);
 }
-
 
 void add_data_to_tree(t_tree **ls_tree, t_file_data *file_data,
 	int (*f)(t_file_data *file1, t_file_data *file2))
--- ftls_tree_comp.c
+++ ftls_tree_comp.c
@@ -18,51 +18,19 @@ int rev_alphanum_comp(t_file_data *file1, t_file_data *file2)
 // add the current path to this function for the Rt flag
 int	 file_date_comp(t_file_data *file1, t_file_data *file2)
 {
-	//stat, lstat
-	//time, ctime
-
-	// char *file_path1;
-	// char *file_path2;
-	// struct stat s1;
-	// struct stat s2;
-	// file_path1 = append_directory(cur_direct, file_name1);
-	// file_path2 = append_directory(cur_direct, file_name2);
-	// //check how the stat, works
-	// stat(file_path1, &s1);
-	// stat(file_path2, &s2);
-
-	// free(file_path1);
-	// free(file_path2);
-	// if(s1.st_ctime - s2.st_ctime == 0)
-	// {
-	// 	return (alphanum_comp(file_name1, file_name2, cur_direct));
-	// }
-	// file1->mod_time - file2->mod_time;
-	// return ((s1.st_ctime - s2.st_ctime) <= 0);
-	return ((file1->mod_time - file2->mod_time) <= 0);
-	//check what is the st_ctime and how it works
+	if(file1->mod_time == file2->mod_time)
+	{
+		return(alphanum_comp(file1, file2));
+	}
+	return ((file1->mod_time - file2->mod_time) < 0);
 }
 
 // when rt flags are used
 int rev_file_date_comp(t_file_data *file1, t_file_data *file2)
 {
-	// char *file_path1;
-	// char *file_path2;
-	// struct stat s1;
-	// struct stat s2;
-	// file_path1 = append_directory(cur_direct, file_name1);
-	// file_path2 = append_directory(cur_direct, file_name2);
-	// //check how the stat, works
-	// stat(file_path1, &s1);
-	// stat(file_path2, &s2);
-
-	// free(file_path1);
-	// free(file_path2);
-	// if(s1.st_mtime - s2.st_mtime == 0)
-	// {
-	// 	return (alphanum_comp(file_name1, file_name2, cur_direct));
-	// }
-	return ((file1->mod_time - file2->mod_time) >= 0);
-	// return ((s1.st_mtime - s2.st_mtime) >= 0);
-	//check what is the st_ctime and how it works
+	if(file1->mod_time == file2->mod_time)
+	{
+		return(alphanum_comp(file1, file2));
+	}
+	return ((file1->mod_time - file2->mod_time) > 0);
 }
